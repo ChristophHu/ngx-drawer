6e3deec53f764fe1562757c56a06f9bc5bf3b37c
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-11-04 20:25:40 +0100

    tests with tailwindcss

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { DrawerComponent } from '../../../ngx-drawer/src/public-api';
 import { IconsComponent } from '@christophhu/ngx-icons';
 
@@ -11,6 +11,17 @@ import { IconsComponent } from '@christophhu/ngx-icons';
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
-export class App {
-  
+export class App implements AfterViewInit {
+  @ViewChild('settingsDrawer') settingsDrawer!: ElementRef<any>
+  divEl: HTMLElement | null = null
+
+  ngAfterViewInit(): void {
+    console.log('settingsDrawer', this.settingsDrawer)
+    this.divEl = this.settingsDrawer.nativeElement
+  }
+  format() {
+    console.log('formatting drawer', this.divEl)
+    this.divEl?.classList.remove('-mr-96')
+    this.divEl?.classList.add('mr-0')
+  }
 }

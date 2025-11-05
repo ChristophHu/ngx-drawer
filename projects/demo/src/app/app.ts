616e8ae8a9bfb6475e7b6e69373e5720a347e64c
ChristophHu <christoph.huschenhoefer@gmx.de>
2025-11-05 09:42:59 +0100

    working drawer

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -12,23 +12,23 @@ import { IconsComponent } from '@christophhu/ngx-icons';
   styleUrl: './app.sass'
 })
 export class App implements AfterViewInit {
-  @ViewChild('settingsDrawer') settingsDrawer!: ElementRef<any>
-  divEl: HTMLElement | null = null
-  open: boolean = false
+  // @ViewChild('settingsDrawer') settingsDrawer!: ElementRef<any>
+  // divEl: HTMLElement | null = null
+  // open: boolean = false
 
   ngAfterViewInit(): void {
-    console.log('settingsDrawer', this.settingsDrawer)
-    this.divEl = this.settingsDrawer.nativeElement
+    // console.log('settingsDrawer', this.settingsDrawer)
+    // this.divEl = this.settingsDrawer.nativeElement
   }
-  format() {
-    if (this.open) {
-      this.divEl?.classList.remove('mr-0')
-      this.divEl?.classList.add('-mr-96')
-      this.open = false
-    } else {
-      this.divEl?.classList.remove('-mr-96')
-      this.divEl?.classList.add('mr-0')
-      this.open = true
-    }
-  }
+  // format() {
+  //   if (this.open) {
+  //     this.divEl?.classList.remove('mr-0')
+  //     this.divEl?.classList.add('-mr-96')
+  //     this.open = false
+  //   } else {
+  //     this.divEl?.classList.remove('-mr-96')
+  //     this.divEl?.classList.add('mr-0')
+  //     this.open = true
+  //   }
+  // }
 }
